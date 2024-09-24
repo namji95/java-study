14f5fe034834f1c1504670ad0ebe989d1038656a
namji95 <skawlgns07@naver.com>
2024-09-24 12:33:00 +0900

    타임리프 기본 사용법

---
+++ thymeleaf-study/src/main/java/com/namji/thymeleafstudy/ThymeleafStudyApplication.java
@@ -0,0 +1,13 @@
+package com.namji.thymeleafstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThymeleafStudyApplication.class, args);
+  }
+
+}
