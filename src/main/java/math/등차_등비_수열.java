4fd1bd0db54528b3f27e3bd662b40edd82b52447
namji95 <skawlgns07@naver.com>
2024-10-06 09:14:50 +0900

    재귀적 방법과 반복적 방법으로 등차, 등비 수열 구하기

--- src/main/java/math/등차_등비_수열.java
+++ src/main/java/math/등차_등비_수열.java
@@ -9,10 +9,11 @@ public class 등차_등비_수열 {
     정의: 등비수열은 각 항이 이전 항에 일정한 값을 곱한 수열입니다. 이 일정한 값을 공비라고 합니다.
      */
 
-    System.out.println("-----------등차수열 구하기-----------");
     int firstTerm = 2;  // 첫 번째 항
-    int commonDifference = 2;  // 공차
+    int commonDifference = 2;  // 공차, 공비
     int n = 5;  // 구하고자 하는 항의 개수
+
+    System.out.println("-----------등차수열 구하기-----------");
     // 재귀적 방법
     System.out.println("-----------재귀-----------");
     arithmeticSequenceRecursion(firstTerm, commonDifference, n);
@@ -20,7 +21,18 @@ public class 등차_등비_수열 {
     // 반복적 방법
     System.out.println("\n-----------반복-----------");
     arithmeticSequenceRepetition(firstTerm, commonDifference, n);
+
+    System.out.println("\n\n-----------등비수열 구하기-----------");
+    // 재귀적 방법
+    System.out.println("-----------재귀-----------");
+    geometricSequenceRecursion(firstTerm, commonDifference, n);
+
+    // 반복적 방법
+    System.out.println("\n-----------반복-----------");
+    geometricSequenceRepetition(firstTerm, commonDifference, n);
   }
+
+  // 등차수열 재귀
   private static void arithmeticSequenceRecursion (int first, int diff, int n) {
     if (n == 0) {
       return;
@@ -29,9 +41,27 @@ public class 등차_등비_수열 {
     arithmeticSequenceRecursion(first + diff, diff, n - 1);
   }
 
+  // 등차수열 반복
   private static void arithmeticSequenceRepetition (int first, int diff, int n) {
     for (int i = 0; i < n; i++) {
       System.out.print(first + i * diff + " ");
+    }
+  }
+
+  // 등비수열 재귀
+  private static void geometricSequenceRecursion (int first, int diff, int n) {
+    if (n == 0) {
+      return;
+    }
+    System.out.print(first + " ");
+    geometricSequenceRecursion(first * diff, diff, n - 1);
+  }
+
+  // 등비수열 반복
+  private static void geometricSequenceRepetition (int first, int diff, int n) {
+    for (int i = 0; i < n; i++) {
+      System.out.print(first + " ");
+      first *= diff;
     }
   }
 }
